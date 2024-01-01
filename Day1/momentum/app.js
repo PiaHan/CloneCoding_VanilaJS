5a8f7dd008d2c3849e7f3f88c106c60521e01c27
PiaHan <78065224+PiaHan@users.noreply.github.com>
2024-01-01 23:11:04 +0900

    Add files via upload

---
+++ Day1/momentum/app.js
@@ -0,0 +1,14 @@
+//alert("Hello")
+console.log(123124);
+
+//cannot change
+const a = 5;
+console.log(a*2);
+
+//can change
+let b = 10;
+console.log(b);
+b = 10;
+console.log(b);
+
+//old variable = var
