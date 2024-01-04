495b7662034818c85743c3aa5c0516d209b9a984
PiaHan <78065224+PiaHan@users.noreply.github.com>
2024-01-04 19:53:08 +0900

    Add files via upload

---
+++ Day2/momentum/app.js
@@ -0,0 +1,28 @@
+//alert("Hello")
+console.log(123124);
+
+//cannot change
+const a = 5;
+console.log(a*2);
+
+//can change
+let b = 10;
+console.log(b);
+b = 10;
+console.log(b);
+
+//old variable = var
+
+//False != null
+//null is a type
+const amI = true;
+console.log(amI);
+
+
+const player = {
+    name = "nico",
+    points: 10,
+    fat : true,
+}
+console.log(player);
+console.log(player.name);
